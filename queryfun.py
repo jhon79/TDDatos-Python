3a2afaef8408b0656c76d2567ee99b04815f8b73
jhon79 <juanlejo@gmail.com>
2023-06-11 18:43:40 -0500

    Query V1

---
+++ queryfun.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_company_information(symbol: str) -> dict:
+    #symbol = "DIS" # Ticker Disney
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{symbol}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    r = requests.get(url, headers = user_agent)
+    #results = r.json()["result"]
+    #print(r.json())
+
+    my_data_selection = {
+        "nombre": r.json()["quoteSummary"]["result"][0]["price"]["shortName"],
+        "ticker": r.json()["quoteSummary"]["result"][0]["price"]["symbol"],
+        "precio": r.json()["quoteSummary"]["result"][0]["summaryDetail"]["previousClose"]["raw"]
+    }
+
+    return my_data_selection
